ff8df7da443efcb2e1c7ad8607a55ce74acbf544
JodiAB <jodiabrahams9@gmail.com>
2025-06-03 17:46:57 +0200

    navBar

---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import NavItems from './NavItems'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar flex items-center justify-between p-4">
+      <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <Image
+            src="/images/logo.svg"
+            alt="logo"
+            width={46}
+            height={44}
+          />
+        </div>
+      </Link>
+
+      <div className="flex items-center gap-8">
+        <NavItems />
+        <p className="cursor-pointer">Sign In</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
