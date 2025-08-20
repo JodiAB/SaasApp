2600071305c40c5748aada185fd67b958249c00a
JodiAB <jodiabrahams9@gmail.com>
2025-08-20 14:45:11 +0200

    clerk Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
@@ -21,8 +29,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
+        <ClerkProvider appearance={{variables: {colorPrimary:'#fe5933'}}}>
+
         <Navbar />
-        {children}</body>
+        {children}
+        </ClerkProvider>
+        </body>
     </html>
   );
 }
